725a8fca030fd72d2802aaebe77fc52301d8144e
kazukgw <kazukgw@gmail.com>
2016-12-12 14:03:20 +0000

    Add start-devenv.sh (start devenv container in host)

---
+++ start-devenv.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# function devenv() {
+cname=$(docker ps -a --format '{{.Image}} {{.Names}} {{.Status}}' | awk 'match($1, /devenv/) {print $2} ')
+if [[ -n "$cname" ]]; then
+  status=$(docker ps -a --format '{{.Image}} {{.Names}} {{.Status}}' | awk 'match($1, /devenv/) {print $3} ')
+  echo ""
+  echo "==> status: $status"
+  if [[ $status =~ ^Up.* ]]; then
+    echo "==> exec $cname"
+    echo ""
+    docker exec -it $cname /bin/bash
+  elif [[ $status =~ ^Exited.* ]]; then
+    echo "==> start and attach $cname"
+    echo ""
+    docker attach $(docker start $cname)
+  else
+    echo "==> undefined status: $status"
+    echo ""
+  fi
+else
+  echo ""
+  echo "==> run new devenv container"
+  echo ""
+  docker-compose -f $DEVENV_COMPOSE_FILE run --name devenv devenv
+fi
+# }
