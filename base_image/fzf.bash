d37bdaa96b21d436900704e11952f329a892d960
kazukgw <kazukgw@gmail.com>
2020-07-07 22:22:52 +0900

    Update

--- base_image/fzf.bash
+++ base_image/fzf.bash
@@ -13,8 +13,3 @@ fi
 # Auto-completion
 # ---------------
 [[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.bash" 2> /dev/null
-
-# Key bindings
-# ------------
-source "$HOME/.fzf/shell/key-bindings.bash"
-
