2bb33bf7a5a5a77b76565a54ab41548fce478993
kazukgw <kazukgw@gmail.com>
2020-09-27 22:06:08 +0900

    Update

--- setup_tools/gcp/setup-gce-instace-ubuntu2004.bash
+++ setup_tools/gcp/setup-gce-instace-ubuntu2004.bash
@@ -2,10 +2,21 @@
 
 # Cloud NAT や Firewall の設定(IAPの Range を Allow しておくなど) は事前に設定済みとする
 
+echo ""
+echo "Step 1 ===>"
+echo ""
+
 sudo apt update
 sudo apt install git vim docker.io docker-compose -y
 
 sudo usermod -aG docker $(whoami)
+
+echo "Please login again to activate the docker-group settings."
+exit 0
+
+echo ""
+echo "Step 2 ==>"
+echo ""
 
 mkdir -p devenv/src
 mkdir -p devenv/bin
