321ea24a6a9bb77c6e11e3981f8967b1eac7f9e4
kazukgw <kazukgw@gmail.com>
2023-01-08 10:22:15 +0900

    Update

--- setup_tools/build_devenv_container.bash
+++
@@ -1,8 +0,0 @@
-docker build -t devenv \
-  --build-arg USER=developer \
-  --build-arg USERID=501 \
-  --build-arg DOCKER_GID=999 \
-  --build-arg PASSWORD=password \
-  --build-arg HOMEDIR=$HOME/devenv \
-  .
-
