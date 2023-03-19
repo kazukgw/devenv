a7a34cac8583719395c7805d6028f3d7216f50fa
kazukgw <kazukgw@gmail.com>
2023-03-19 20:46:06 +0900

    Update

--- dotfiles/wezterm.lua
+++ dotfiles/wezterm.lua
@@ -4,6 +4,17 @@ return {
     {family = 'HackGen35 Console NFJ' },
   },
   color_scheme = "Sonokai (Gogh)",
+  -- Overrides the cell background color when the current cell is occupied by the
+  colors = {
+    -- cursor and the cursor style is set to Block
+    cursor_bg = '#5A6477',
+    -- Overrides the text color when the current cell is occupied by the cursor
+    cursor_fg = 'white',
+    -- Specifies the border color of the cursor when the cursor style is set to Block,
+    -- or the color of the vertical or horizontal bar when the cursor style is set to
+    -- Bar or Underline.
+    cursor_border = '#E1E3E4',
+  },
   hide_tab_bar_if_only_one_tab = true,
   window_padding = {
     left = 0,
